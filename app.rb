95511d539310da770546983babf62c7ad247cc75
sd-kin <sd-kin@rambler.ru>
2015-07-26 19:42:01 +0300

    firststep

--- app.rb
+++ app.rb
@@ -5,3 +5,25 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/about' do
+erb :about
+end
+
+get '/visit' do
+erb :visit
+end
+
+get '/contacts' do
+erb :contacts
+end
+
+post '/visit' do
+@name=params[:name]
+@phone=params[:phone]
+@date=params[:date]
+file=File.open"public/list.txt", "a"
+file<<"#{@name} wont to visit you at #{@date}, phon  number - #{@phone}\n"
+file.close
+erb :visit
+end
