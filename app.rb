104cf69de442ea468564adedde1e527b4b0b2243
sd-kin <sd-kin@rambler.ru>
2015-07-27 17:20:06 +0300

    login page

--- app.rb
+++ app.rb
@@ -2,6 +2,24 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
+
+
+
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+
+
+redirrect '/'
+
+end
+
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
